3190ae6462c2045b02a1e7c047710486112a0147
gr3tchel <maioranosimona@yahoo.it>
2018-01-27 01:15:31 +0100

    Commit

---
+++ Transmission/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+
+	private Rigidbody2D rb2d;       
+
+
+	void Start()
+	{
+
+		rb2d = GetComponent<Rigidbody2D> ();
+	}
+
+
+	void FixedUpdate()
+	{
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		//Use the two store floats to create a new Vector2 variable movement.
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+
+		rb2d.AddForce (movement * speed);
+	}
+}
